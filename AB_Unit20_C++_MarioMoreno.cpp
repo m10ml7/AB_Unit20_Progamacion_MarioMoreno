253ed4c65ec0209f97a55ead7f824e9c076928ae
m10ml7 <marioml@msmk.university>
2024-11-26 19:05:29 +0100

    Commit menú citas

--- AB_Unit20_C++_MarioMoreno.cpp
+++ AB_Unit20_C++_MarioMoreno.cpp
@@ -7,6 +7,9 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <cstdlib>
+#include <ctime>
+#include <set>
 
 using namespace std;
 
@@ -103,14 +106,15 @@ public:
 
 class CitaMedica {
 public:
+    string id;
     string fechaHora;
     string especialidad;
     Medico* medico;
     Paciente* paciente;
-    int nivelUrgencia;
+    short int nivelUrgencia;
 
-    CitaMedica(string fechaHora, string especialidad, Medico* medico, Paciente* paciente, int nivelUrgencia)
-        : fechaHora(fechaHora), especialidad(especialidad), medico(medico), paciente(paciente), nivelUrgencia(nivelUrgencia) {}
+    CitaMedica(string id, string fechaHora, string especialidad, Medico* medico, Paciente* paciente, short int nivelUrgencia)
+        :id(id), fechaHora(fechaHora), especialidad(especialidad), medico(medico), paciente(paciente), nivelUrgencia(nivelUrgencia) {}
 
     void modificarCita() {
         cout << "Modificar datos de la cita.\n";
@@ -129,6 +133,35 @@ public:
             });
     }
 };
+
+// Almacenamientos de IDs únicos
+set<string> idGenerar;
+
+string generarIDCita(Paciente* paciente) {
+    // Obtener iniciales del paciente
+    string iniciales;
+    if (!paciente->nombre.empty())
+        iniciales += toupper(paciente->nombre[0]);
+    if (!paciente->apellidos.empty()) {
+        auto espacio = paciente->apellidos.find(' ');
+        iniciales += toupper(paciente->apellidos[0]);
+        if (espacio != string::npos && espacio + 1 < paciente->apellidos.length())
+            iniciales += toupper(paciente->apellidos[espacio + 1]);
+    }
+
+    // Generar número aleatorio
+    string idCita;
+    srand(time(nullptr)); 
+    do {
+        int numero = rand() % 10000; // Generar número aleatorio de 4 cifras
+        char numeroStr[5];
+        snprintf(numeroStr, sizeof(numeroStr), "%04d", numero);
+        idCita = string(numeroStr) + " " + iniciales;
+    } while (idGenerar.count(idCita)); // Repetir si el ID ya existe
+
+    idGenerar.insert(idCita); 
+    return idCita;
+}
 
 class Servicios {
 public:
@@ -179,16 +212,18 @@ void menuPacientes(vector<Paciente>& pacientes) {
             string nombre, apellidos, dni, fechaIngreso, enfermedad;
             cout << "Ingrese nombre: ";
             cin >> nombre;
+            cin.ignore();
             cout << "Ingrese apellidos: ";
-            cin >> apellidos;
+            getline(cin, apellidos);
             cout << "Ingrese DNI: ";
             cin >> dni;
-            cout << "Ingrese fecha de ingreso: ";
-            cin >> fechaIngreso;
+            cout << "Ingrese fecha de ingreso (si hay): ";
+            cin.ignore();
+            getline(cin, fechaIngreso);
             cout << "Ingrese enfermedad (opcional): ";
             cin.ignore();
             getline(cin, enfermedad);
-            pacientes.emplace_back(nombre, apellidos, dni, fechaIngreso, enfermedad);
+            pacientes.emplace_back(nombre, apellidos, dni, fechaIngreso, enfermedad); 
             cout << "Paciente registrado.\n";
             break;
         }
@@ -343,6 +378,7 @@ void menuPacientes(vector<Paciente>& pacientes) {
     } while (opcion != 8);
 }
 
+// Menú médicos
 void menuMedicos(vector<Medico>& medicos) {
     int opcion;
     do {
@@ -498,6 +534,275 @@ void menuMedicos(vector<Medico>& medicos) {
     } while (opcion != 8);
 }
 
+// Menú citas médicas
+void menuCitas(vector<CitaMedica>& citas, vector<Medico>& medicos, vector<Paciente>& pacientes) {
+    int opcion;
+    do {
+        cout << "--- Menu citas ---\n";
+        cout << "1. Asignar cita\n";
+        cout << "2. Modificar datos de la cita\n";
+        cout << "3. Cancelar cita\n";
+        cout << "4. Citas ordenadas por fecha o nivel de urgencia\n";
+        cout << "5. Registro de citas completo\n";
+        cout << "6. Volver al menu principal\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        switch (opcion) {
+        case 1: {
+            string id;
+            string fechaHora, especialidad;
+            short int nivelUrgencia;
+            Medico* medico = nullptr;
+            Paciente* paciente = nullptr;
+
+            cout << "Asignar una nueva cita\n";
+            cout << "Fecha y hora (YYYY-MM-DD HH:MM): ";
+            cin.ignore();
+            getline(cin, fechaHora);
+
+            vector<string> especialidades = {
+                "Alergologia", "Anatomia Patologica", "Anestesiologia y Reanimacion",
+                "Angiologia y Cirugia Vascular", "Aparato Digestivo", "Cardiologia",
+                "Cirugia Cardiovascular", "Cirugia General y del Aparato Digestivo",
+                "Cirugia Oral y Maxilofacial", "Cirugia Ortopedica y Traumatologia",
+                "Cirugia Pediatrica", "Cirugia Plastica, Estetica y Reparadora",
+                "Cirugía Toracica", "Dermatologia Medico-Quirurgica y Venereologia",
+                "Endocrinologia y Nutricion", "Farmacologia Clinica", "Geriatria",
+                "Hematologia y Hemoterapia", "Inmunologia", "Medicina del Trabajo",
+                "Medicina Familiar y Comunitaria", "Medicina Fisica y Rehabilitacion",
+                "Medicina Intensiva", "Medicina Interna", "Medicina Nuclear",
+                "Medicina Preventiva y Salud Publica", "Nefrologia", "Neumologia",
+                "Neurocirugia", "Neurofisiologia Clinica", "Neurologia",
+                "Obstetricia y Ginecologia", "Oftalmologia", "Oncologia Medica",
+                "Oncología Radioterapica", "Otorrinolaringologia",
+                "Pediatria y sus Areas Especificas", "Psiquiatria", "Radiodiagnostico",
+                "Reumatologia", "Urologia"
+            };
+
+            cout << "Elija una especialidad:\n";
+            for (size_t i = 0; i < especialidades.size(); ++i) {
+                cout << i + 1 << ". " << especialidades[i] << endl;
+            }
+
+            int especialidadSeleccion;
+            cout << "Seleccione el numero de la especialidad (0 para cancelar): ";
+            cin >> especialidadSeleccion;
+
+            if (especialidadSeleccion < 1 || especialidadSeleccion > especialidades.size()) {
+                cout << "Especialidad invalida. Operacion cancelada.\n";
+                break;
+            }
+
+            especialidad = especialidades[especialidadSeleccion - 1];
+
+            // Seleccionar médico
+            cout << "Medicos disponibles de la especialidad " << especialidad << ":\n";
+            vector<Medico*> medicosEspecialidad;
+            for (auto& medicoIter : medicos) {
+                if (medicoIter.especialidad == especialidad && medicoIter.disponible) {
+                    medicosEspecialidad.push_back(&medicoIter);
+                }
+            }
+
+            if (medicosEspecialidad.empty()) {
+                cout << "No hay medicos disponibles para esta especialidad.\n";
+                break;
+            }
+
+            for (size_t i = 0; i < medicosEspecialidad.size(); ++i) {
+                cout << i + 1 << ". " << medicosEspecialidad[i]->nombre << " " << medicosEspecialidad[i]->apellidos << endl;
+            }
+
+            int medicoSeleccion;
+            cout << "Seleccione el numero del medico (0 para cancelar): ";
+            cin >> medicoSeleccion;
+
+            if (medicoSeleccion > 0 && medicoSeleccion <= medicosEspecialidad.size()) {
+                medico = medicosEspecialidad[medicoSeleccion - 1];
+            }
+            else {
+                cout << "Selección invalida. Operacion cancelada.\n";
+                break;
+            }
+
+            // Seleccionar paciente
+            cout << "Lista de pacientes registrados:\n";
+            for (size_t i = 0; i < pacientes.size(); ++i) {
+                cout << i + 1 << ". " << pacientes[i].nombre << " " << pacientes[i].apellidos << " - DNI: " << pacientes[i].dni << endl;
+            }
+
+            int pacienteSeleccion;
+            cout << "Seleccione el numero del paciente (0 para cancelar): ";
+            cin >> pacienteSeleccion;
+
+            if (pacienteSeleccion > 0 && pacienteSeleccion <= pacientes.size()) {
+                paciente = &pacientes[pacienteSeleccion - 1];
+            }
+            else {
+                cout << "Seleccion invalida. Operacion cancelada.\n";
+                break;
+            }
+
+            // Generar ID único dependiendo de las inciales del paciente
+            id = generarIDCita(paciente);
+
+            cout << "Nivel de urgencia (1-5, siendo 5 el mas urgente): ";
+            cin >> nivelUrgencia;
+
+            if (nivelUrgencia < 1 || nivelUrgencia > 5) {
+                cout << "Nivel de urgencia invalido. Operacion cancelada.\n";
+                break;
+            }
+
+            // Crear la cita y agregarla a la lista
+            citas.emplace_back(id, fechaHora, especialidad, medico, paciente, nivelUrgencia);
+            cout << "Cita asignada exitosamente.\n";
+            cout << "ID: " << id << ", Fecha: " << fechaHora << ", Especialidad: " << especialidad;
+            cout << ", Medico: " << medico->nombre << " " << medico->apellidos;
+            cout << ", Paciente: " << paciente->nombre << " " << paciente->apellidos;
+            cout << ", Nivel de urgencia: " << nivelUrgencia << endl;
+            break;
+        }
+        case 2: {
+            string id;
+            cout << "Ingrese el ID de la cita a modificar: ";
+            cin >> id;
+            for (auto& cita : citas) {
+                if (cita.id == id) {
+                    cita.modificarCita();
+                    cout << "Cita modificada con exito.\n";
+                    break;
+                }
+            }
+            break;
+        }
+        case 3: {
+            string nombre, apellidos;
+            cout << "Ingrese el nombre y apellidos del paciente o medico: ";
+            cin >> nombre >> apellidos;
+
+            vector<CitaMedica*> coincidencias;
+            for (auto& cita : citas) {
+                if (cita.medico->nombre == nombre && cita.medico->apellidos == apellidos ||
+                    cita.paciente->nombre == nombre && cita.paciente->apellidos == apellidos) {
+                    coincidencias.push_back(&cita);
+                }
+            }
+
+            if (coincidencias.empty()) {
+                cout << "No se encontraron coincidencias.\n";
+                break;
+            }
+
+            for (size_t i = 0; i < coincidencias.size(); ++i) {
+                cout << i + 1 << ". Cita ID: " << coincidencias[i]->id << " - Fecha: " << coincidencias[i]->fechaHora
+                    << " - Medico: " << coincidencias[i]->medico->nombre << " " << coincidencias[i]->medico->apellidos
+                    << " - Paciente: " << coincidencias[i]->paciente->nombre << " " << coincidencias[i]->paciente->apellidos << endl;
+            }
+
+            int seleccion;
+            cout << "Seleccione el numero de la cita a cancelar: ";
+            cin >> seleccion;
+
+            if (seleccion > 0 && seleccion <= coincidencias.size()) {
+                auto it = remove_if(citas.begin(), citas.end(), [&](CitaMedica& cita) {
+                    return &cita == coincidencias[seleccion - 1];
+                    });
+                citas.erase(it, citas.end());
+                cout << "Cita cancelada con exito.\n";
+            }
+            else {
+                cout << "Seleccion invalida.\n";
+            }
+            break;
+        }
+        case 4: {
+            short int subopciones;
+            cout << "Elige una opcion para ordenar las citas:\n";
+            cout << "1. Ordenar por fecha\n";
+            cout << "2. Ordenar por nivel de urgencia\n";
+            cout << "Opcion: ";
+            cin >> subopciones;
+
+            time_t t = time(nullptr);
+            tm* now = localtime(&t);
+            char currentDate[20];
+            strftime(currentDate, sizeof(currentDate), "%Y-%m-%d %H:%M", now);
+
+            if (subopciones == 1) {
+                sort(citas.begin(), citas.end(), [](const CitaMedica& a, const CitaMedica& b) {
+                    return a.fechaHora < b.fechaHora;
+                    });
+
+                cout << "Citas ordenadas por fecha (solo futuras):\n";
+                for (const auto& cita : citas) {
+                    if (cita.fechaHora >= currentDate) { // Filtrar citas futuras
+                        cout << "ID: " << cita.id
+                            << " - Fecha: " << cita.fechaHora
+                            << " - Paciente: " << cita.paciente->nombre << " " << cita.paciente->apellidos
+                            << " - Medico: " << cita.medico->nombre << " " << cita.medico->apellidos
+                            << " - Nivel de urgencia: " << cita.nivelUrgencia << endl;
+                    }
+                }
+            }
+            else if (subopciones == 2) {
+                sort(citas.begin(), citas.end(), [](const CitaMedica& a, const CitaMedica& b) {
+                    return a.nivelUrgencia > b.nivelUrgencia;
+                    });
+
+                cout << "Citas ordenadas por nivel de urgencia (más urgentes primero, solo futuras):\n";
+                for (const auto& cita : citas) {
+                    if (cita.fechaHora >= currentDate) {
+                        cout << "ID: " << cita.id
+                            << " - Fecha: " << cita.fechaHora
+                            << " - Nivel de urgencia: " << cita.nivelUrgencia
+                            << " - Paciente: " << cita.paciente->nombre << " " << cita.paciente->apellidos
+                            << " - Medico: " << cita.medico->nombre << " " << cita.medico->apellidos << endl;
+                    }
+                }
+            }
+            else {
+                cout << "Opcion invalida.\n";
+            }
+            break;
+        }
+        case 5: {
+            time_t t = time(nullptr);
+            tm* now = localtime(&t);
+            char currentDate[20];
+            strftime(currentDate, sizeof(currentDate), "%Y-%m-%d %H:%M", now);
+
+            short int subopciones;
+            cout << "Elige una opcion:\n1. Citas pasadas\n2. Citas futuras\nOpcion: ";
+            cin >> subopciones;
+
+            if (subopciones == 1) {
+                cout << "Citas pasadas:\n";
+                for (const auto& cita : citas) {
+                    if (cita.fechaHora < currentDate)
+                        cout << "ID: " << cita.id << " - Fecha: " << cita.fechaHora << " - Paciente: " << cita.paciente->nombre << endl;
+                }
+            }
+            else if (subopciones == 2) {
+                cout << "Citas futuras:\n";
+                for (const auto& cita : citas) {
+                    if (cita.fechaHora >= currentDate)
+                        cout << "ID: " << cita.id << " - Fecha: " << cita.fechaHora << " - Paciente: " << cita.paciente->nombre << " - Medico: " << cita.medico->nombre << endl;
+                }
+            }
+            else {
+                cout << "Opcion invalida.\n";
+            }
+            break;
+        }
+        case 6:
+            break;
+        default:
+            cout << "Opcion no valida.\n";
+        }
+    } while (opcion != 6);
+}
 
 // Menú opciones
 void menuPrincipal() {
@@ -525,7 +830,7 @@ void menuPrincipal() {
             menuMedicos(medicos);
             break;
         case 3:
-           // menuCitas(citas);
+            menuCitas(citas, medicos, pacientes);
             break;
         case 4:
            // menuServicios(servicios);
